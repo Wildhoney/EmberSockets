41e8473455304886328886f608383d0c38af3f51
Adam Timberlake <adam.timberlake@gmail.com>
2014-07-07 22:22:21 +0100

    Added Express with Socket.io

--- example/server.js
+++ example/server.js
@@ -1,36 +1,36 @@
-var io          = require('socket.io').listen(8888),
-    fs          = require('fs'),
-    _           = require('underscore');
-
-/**
- * @on connection
- */
-io.sockets.on('connection', function (socket) {
-    /**
-     * @on cherryPickName
-     * Updates the content when the `applyFilterByWord` event has been received.
-     */
-    socket.on('cherryPickName', function(name, age) {
-
-        var names = ['Adam', 'Masha', 'Baki', 'Vaidas', 'Dhruv', 'Gabriele', 'Javier',
-                     'Noemi', 'Dmitri', 'Simon', 'Artem', 'Raj', 'Mark', 'Mide'];
-
-        socket.emit('cherryPickedName', _.sample(names), Math.floor(Math.random() * 30) + 1);
-
-    });
-
-});
-
 (function($process) {
 
     "use strict";
 
     var express     = require('express'),
         app         = express(),
-        server      = require('http').createServer(app);
+        server      = require('http').createServer(app),
+        io          = require('socket.io').listen(server),
+        fs          = require('fs');
 
     // Begin Express so the statistics are available from the `localPort`.
     app.use(express.static(__dirname + '/../example'));
     server.listen($process.env.PORT || 3502);
+
+    /**
+     * @on connection
+     */
+    io.sockets.on('connection', function (socket) {
+
+        /**
+         * Updates the content when the `applyFilterByWord` event has been received.
+         *
+        * @on cherryPickName
+        */
+        socket.on('cherryPickName', function(name, age) {
+
+            var names = ['Adam', 'Masha', 'Baki', 'Vaidas', 'Dhruv', 'Gabriele', 'Javier',
+                         'Noemi', 'Dmitri', 'Simon', 'Artem', 'Raj', 'Mark', 'Mide'];
+
+            socket.emit('cherryPickedName', _.sample(names), Math.floor(Math.random() * 30) + 1);
+
+        });
+
+    });
 
 })(process);
