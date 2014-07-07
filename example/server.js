beca10b27ca0c7537e21f91aa3af6fdbebb1bbeb
Adam Timberlake <adam.timberlake@gmail.com>
2014-07-07 22:06:33 +0100

    Added Node.js server

--- example/server.js
+++ example/server.js
@@ -20,3 +20,17 @@ io.sockets.on('connection', function (socket) {
     });
 
 });
+
+(function($process) {
+
+    "use strict";
+
+    var express     = require('express'),
+        app         = express(),
+        server      = require('http').createServer(app);
+
+    // Begin Express so the statistics are available from the `localPort`.
+    app.use(express.static(__dirname + '/../example'));
+    server.listen($process.env.PORT || 3502);
+
+})(process);
